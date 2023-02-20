44d3d21be6a4e7ae3f99549f5e00c5bbfbd49a83
Christian Harke <christian@harke.ch>
2023-02-20 22:34:46 +0100

    Add http url validation to repository

--- repositories/emojos.js
+++ repositories/emojos.js
@@ -2,8 +2,23 @@
 
 const httpClient = require('./utils/httpclient');
 
+const addProtocolFallback = string => {
+  const hasProtocol = string.includes('://');
+  return hasProtocol ? string : `https://${string}`;
+}
+
+const validatedUrl = string => {
+  const url = new URL(addProtocolFallback(string));
+  if (url.protocol === 'http:' || url.protocol === 'https:') {
+    return url;
+  }
+  throw new Error('Argument is not a HTTP/S URL!');
+}
+
 const fetchEmojos = instance => {
-  return httpClient.getJson(`https://${instance}/api/v1/custom_emojis`);
+  const url = validatedUrl(instance);
+  const emojoApi = new URL(`/api/v1/custom_emojis`, url);
+  return httpClient.getJson(emojoApi);
 };
 
 module.exports = {
--- test/repositories/emojos.test.js
+++ test/repositories/emojos.test.js
@@ -1,14 +1,30 @@
 'use strict'
 
-const {mock, test} = require('tap');
+const {mock, same, test} = require('tap');
 
 const HttpClientMock = require('../mocks/repositories/utils/httpclient');
 
 const repo = mock('../../repositories/emojos', {
   '../../repositories/utils/httpclient': HttpClientMock,
-})
+});
 
-test('emojos can be fetched from a valid url', async t => {
-  const actual = await repo.fetchEmojos('mymastodon.com');
-  t.same(actual, HttpClientMock.resultStub);
+const testUrl = async (url) => {
+  const actual = await repo.fetchEmojos(url);
+  same(actual, HttpClientMock.resultStub);
+};
+
+test('urls without protocol should be supported', async t => {
+  await testUrl('mymastodon.com');
+});
+
+test('http urls should be supported', async t => {
+  await testUrl('http://mymastodon.com');
+});
+
+test('https urls should be supported', async t => {
+  await testUrl('https://mymastodon.com');
+});
+
+test('non http/s urls should not be supported', async t => {
+  t.throws(() => repo.fetchEmojos('webcal://mymastodon.com'));
 });
